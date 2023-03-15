c549c71fa2f86cca3aba4ae390af015b2ef80a7c
Hamza Tural <hamzatural14@gmail.com>
2023-03-15 09:25:59 +0300

    LLL

--- WindowsFormsApp8/Form1.Designer.cs
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -34,12 +34,13 @@ namespace WindowsFormsApp8
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(131, 120);
+            this.button1.Location = new System.Drawing.Point(197, 137);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(313, 129);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
