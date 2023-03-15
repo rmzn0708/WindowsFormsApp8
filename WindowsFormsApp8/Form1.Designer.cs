3e8bd6f6367248a704a9603b1238e96865c4ad52
rmzn0708 <dnmders0708@gmail.com>
2023-03-15 10:05:00 +0300

    ĞFGDFD

--- WindowsFormsApp8/Form1.Designer.cs
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -34,11 +34,11 @@ namespace WindowsFormsApp8
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(197, 137);
+            this.button1.Location = new System.Drawing.Point(174, 167);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(313, 129);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "DENEME 3";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
